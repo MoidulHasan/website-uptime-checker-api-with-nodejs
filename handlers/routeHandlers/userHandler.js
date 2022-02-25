eea568dc848df62659130181aa13cd7d8d208d43
MoidulHasan <antu.khan.988@gmail.com>
2022-02-26 04:48:06 +0600

    CURD API for user completed

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -81,15 +81,15 @@ handler._users.post = (requestProperty, callback) => {
 }
 
 handler._users.get = (requestProperty, callback) => {
-    // validete second name
+    // validete phone
     const phone = typeof(requestProperty.queryObject.phone) === "string" && requestProperty.queryObject.phone.trim().length === 11 ? requestProperty.queryObject.phone : false;
 
     if (phone) {
         data.read("users", phone, (err, data) => {
-            const user = parseJSON(data);
-            delete user.password;
-            console.log(data)
-            if (!err && user) {
+            if (!err && data) {
+                const user = parseJSON(data);
+                delete user.password;
+
                 callback(200, user);
             } else {
                 callback(404, {
@@ -106,9 +106,96 @@ handler._users.get = (requestProperty, callback) => {
 
 handler._users.put = (requestProperty, callback) => {
 
+    // validete first name
+    const firstName = typeof(requestProperty.body.firstName) === "string" && requestProperty.body.firstName.trim().length > 0 ? requestProperty.body.firstName : false;
+
+    // validete lastName name
+    const lastName = typeof(requestProperty.body.lastName) === "string" && requestProperty.body.lastName.trim().length > 0 ? requestProperty.body.lastName : false;
+
+    // validete phone
+    const phone = typeof(requestProperty.body.phone) === "string" && requestProperty.body.phone.trim().length === 11 ? requestProperty.body.phone : false;
+
+    // validete password
+    const currentPassword = typeof(requestProperty.body.currentPassword) === "string" && requestProperty.body.currentPassword.trim().length > 0 ? requestProperty.body.currentPassword : false;
+
+    // validete password
+    const newPassword = typeof(requestProperty.body.newPassword) === "string" && requestProperty.body.newPassword.trim().length > 0 ? requestProperty.body.newPassword : false;
+
+    if (phone && currentPassword && (firstName || lastName || newPassword)) {
+        data.read("users", phone, (err, userData) => {
+            if (!err) {
+                const user = parseJSON(userData);
+                if (hash(currentPassword) === user.password) {
+                    // update data to user object if requested to update
+                    if (firstName) {
+                        user.firstName = firstName;
+                    }
+
+                    if (lastName) {
+                        user.lastName = lastName;
+                    }
+
+                    if (newPassword) {
+                        user.password = hash(newPassword);
+                    }
+
+                    data.update("users", phone, user, (Message, err) => {
+                        if (!err) {
+                            callback(200, {
+                                message: Message
+                            })
+                        } else {
+                            callback(400, {
+                                error: Message
+                            });
+                        }
+                    })
+                } else {
+                    callback(404, {
+                        error: "Could not update user data, Current password dosen't match."
+                    })
+                }
+            } else {
+                callback(404, {
+                    error: "User not found."
+                })
+            }
+        })
+    } else {
+        callback(404, {
+            error: "Could not update data, please provide valid data."
+        })
+    }
 }
 
 handler._users.delete = (requestProperty, callback) => {
+    // validete phone
+    const phone = typeof(requestProperty.body.phone) === "string" && requestProperty.body.phone.trim().length === 11 ? requestProperty.body.phone : false;
 
+    if (phone) {
+        data.read("users", phone, (err, userData) => {
+            if (!err) {
+                data.delete("users", phone, (err) => {
+                    if (!err) {
+                        callback(200, {
+                            message: "User Deleted Successfully"
+                        });
+                    } else {
+                        callback(400, {
+                            message: "User could not deleted."
+                        });
+                    }
+                });
+            } else {
+                callback(404, {
+                    error: "User dosen't exist."
+                });
+            }
+        });
+    } else {
+        callback(404, {
+            error: "Could not update data, please provide valid phone number."
+        });
+    }
 }
 module.exports = handler;
